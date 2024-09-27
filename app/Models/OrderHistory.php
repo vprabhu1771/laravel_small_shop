b426758485d03e822542761c243ce20b8c1a77bc
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2024-09-27 09:33:55 +0530

    Order History migration and model

---
+++ app/Models/OrderHistory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderHistory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'order_status',
+        'payment_method'       
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class, 'order_id');
+    }
+    
+}
