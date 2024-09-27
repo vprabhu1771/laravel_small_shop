b8da33db57ee071f1ea8a96e9093a01039a6ad31
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2024-09-27 09:33:45 +0530

    Order Item migration and model

---
+++ database/migrations/2024_09_27_040054_create_order_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('product_id');
+            $table->integer('qty');
+            $table->decimal('unit_price', 10, 2);
+            $table->decimal('amount', 10, 2);
+            $table->integer('discount')->default(0);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
