dffe9c7e6a63ba8b1b0e0d5861e192856d47a924
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2024-09-27 09:26:39 +0530

    Product migration and model

---
+++ app/Models/Product.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Support\Facades\Storage;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'category_id',
+        'brand_id',
+        'qty',
+        'id',
+        'alert_stock',
+        'image_path',
+        'description'
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function GetImagePath()
+    {
+        return env('DOMAIN_URL') . Storage::url($this->image_path);
+    }
+}
---
+++ database/migrations/2024_09_27_035400_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->decimal('price',10,2)->nullable();
+            
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('brand_id');
+            
+            $table->string('image_path')->default('no_image_available.jpg')->nullable();
+            $table->integer('qty')->nullable();
+            
+            $table->integer('alert_stock')->default(0);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
